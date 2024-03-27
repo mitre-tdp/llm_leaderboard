134f00377c199cd43b1e39bea0f936c492c8da02
Karl Kwon <kkwon@mitre.org>
2024-03-27 11:04:36 -0400

    update help texts

--- help.py
+++ help.py
@@ -25,8 +25,33 @@ The leaderboard data is loaded from a CSV file. You can modify the data in `lead
 Sorting and filtering functionality might be limited depending on the size of your data.
 """
 
+F1_SCORE_DEFINITION = """
+***F1 Score*** is a metric that combines precision and recall. It is calculated as the harmonic mean of precision and recall:
+- Precision: The proportion of true positives among the predicted positives.
+- Recall: The proportion of true positives among the actual positives.
+
+A higher F1 Score indicates a better balance between precision and recall.
+"""
+
+EXACT_MATCH_SCORE_DEFINITION = """
+***Exact Match Score*** is the percentage of predictions where all words exactly match the ground truth labels. It measures how well the model predicts the exact sequence of words in the reference answer.
+
+A higher Exact Match Score indicates that the model's predictions are more likely to be identical to the correct answer.
+"""
+
+DISCLAIMER = """
+YOUR DISCLAIMER TEXT HERE
+"""
+
 
 def display_help_page():
     """Displays the help page."""
-    st.subheader("Help")
+    st.subheader("About")
     st.markdown(HELP_TEXT)
+
+    st.subheader("Definition")
+    st.markdown(F1_SCORE_DEFINITION)  # Display F1 Score definition
+    st.markdown(EXACT_MATCH_SCORE_DEFINITION)  # Display Exact Match Score definition
+
+    st.subheader("Disclaimer")
+    st.markdown(DISCLAIMER)  # Display Exact Match Score definition
